17000eb57dd300266fe08b1f992b559835da74f5
jotkafomat <k-j@xl.wp.pl>
2020-11-24 18:18:20 +0000

    add Nav Menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,12 +2,38 @@ import React from 'react'
 
 export default function Navigation() {
 
+  const navLinks = [
+    {
+      title: 'Home',
+      path: '/'
+    },
+    {
+      title: 'Post',
+      path: '/post'
+    },
+    {
+      title: 'Log in',
+      path: '/log_in'
+    },
+    {
+      title: 'Log out',
+      path: '/log_out'
+    },
+    {
+      title: 'Sign in',
+      path: '/sign_in'
+    }
+  ]
+
   return (
     <nav className="site-navigation">
       <span>BruhBook</span>
       <ul>
-        <li>
-        </li>
+        { navLinks.map((link, index) => (
+          <li key={index}>
+              {link.title}
+          </li>
+        ))}
       </ul>
     </nav>)
 }
