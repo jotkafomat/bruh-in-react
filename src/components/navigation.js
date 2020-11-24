858dc57a9cc29fe4f05cd7cde041779c0e2cc2c8
jotkafomat <k-j@xl.wp.pl>
2020-11-24 17:50:12 +0000

    add Navigation Bar

---
+++ src/components/navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navigation() {
+
+  return (
+    <nav className="site-navigation">
+      <span>BruhBook</span>
+      <ul>
+        <li>
+        </li>
+      </ul>
+    </nav>)
+}
