9860e4f5653b3762c2d66b50365c3a5466d698a5
jotkafomat <k-j@xl.wp.pl>
2020-11-24 20:14:26 +0000

    add links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navigation from './components/navigation'
+import {BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+      </div>
+    </Router>
   );
 }
 
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import{Link} from 'react-router-dom';
 
-export default function Navigation() {
 
   const navLinks = [
     {
@@ -25,13 +25,14 @@ export default function Navigation() {
     }
   ]
 
+  export default function Navigation() {
   return (
     <nav className="site-navigation">
       <span>BruhBook</span>
       <ul>
         { navLinks.map((link, index) => (
           <li key={index}>
-              {link.title}
+              <Link to={link.path}>{link.title}</Link>
           </li>
         ))}
       </ul>
